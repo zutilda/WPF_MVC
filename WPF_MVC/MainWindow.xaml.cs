14677dbcb06634ead3486b79e42558b9b32c3900
Настя Жулина <julina.nastia2017@yandex.ru>
2023-01-13 18:19:33 +0300

    Добавьте файлы проекта.

---
+++ WPF_MVC/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_MVC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Models.tbOne = One;
+            Models.tbTwo = Two;
+            Models.tbOperation = Operation;
+
+
+        }
+
+        private void cbOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            Models.Combo = cbOperation.SelectedIndex;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Models.ResultOne = Convert.ToDouble(One.Text);
+            Models.ResultTwo = Convert.ToDouble(Two.Text);
+            Models.Result = cbOperation.SelectedIndex;
+        }
+    }
+}
---
+++ WPF_MVC/Models.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_MVC
+{
+    public static class Models
+    {
+        public static double one;
+        public static double two;
+        public static List<string> operationList = new List<string> { "+", "-", "*", "/" };
+
+        public static TextBox tbOne;
+        public static TextBox tbTwo;
+        public static TextBlock tbOperation;
+        public static GroupBox res;
+        public static double ResultOne
+        {
+            get { return one; }
+            set
+            {
+                one = value;
+                tbOne.Text = one.ToString();
+            }
+        }
+        public static double ResultTwo
+        {
+            get { return two; }
+            set
+            {
+                two = value;
+                tbTwo.Text = two.ToString();
+            }
+        }
+
+        public static int Combo
+        {
+            set
+            {
+                tbOperation.Text = operationList[value];
+            }
+        }
+
+        public static int Result 
+        {
+            set
+            {
+                switch (operationList)
+                {
+                    case 0:
+                        break;
+                }
+            }
+        }
+
+    }
+}
